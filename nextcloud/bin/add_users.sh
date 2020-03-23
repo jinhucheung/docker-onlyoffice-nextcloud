a2e86dc03365c56d61d4bde88eb03834d3068d9f
jimcheung <hi.jinhu.zhang@gmail.com>
2020-03-23 23:51:05 +0800

    feat: add default group to csv

--- nextcloud/bin/add_users.sh
+++ nextcloud/bin/add_users.sh
@@ -4,7 +4,7 @@ csv_file_path=$1
 default_password=$2
 
 IFS=","
-while read username name email group || [[ -n $username ]]; do
-  docker exec -u www-data -e "OC_PASS=$default_password" nextcloud-app php occ user:add $username --password-from-env --group="$group" --display-name="$name"
+while read username name email default_group group || [[ -n $username ]]; do
+  docker exec -u www-data -e "OC_PASS=$default_password" nextcloud-app php occ user:add $username --password-from-env --group="$group" --group="$default_group" --display-name="$name"
   docker exec -u www-data nextcloud-app php occ user:setting $username settings email "$email"
 done < $csv_file_path
